94c5d29723361682cf81c48fd0643c3013e993d6
shakalandro <shakalandro@gmail.com>
2011-10-14 20:43:39 -0700

    added template dependency

--- src/server.py
+++ src/server.py
@@ -6,14 +6,17 @@ Created on Oct 14, 2011
 
 import socket
 import BaseHTTPServer
+import index
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         client_host, client_port = self.client_address
         print 'Serving %s to %s:%s' % (self.path, client_host, client_port)
+        self.send_response(200)
         self.send_header('Content-Type', 'text/html')
-        self.wfile.write('Hello World!')
+        self.end_headers()
+        self.wfile.write(index.get_index())
         
     def do_POST(self):
         self.send_header('Content-Type', 'text/html')
@@ -23,7 +26,7 @@ def main():
     port = 80
     try:
         server = BaseHTTPServer.HTTPServer(("", port), Handler)
-    except:
+    except socket.error:
         port = 8888
         server = BaseHTTPServer.HTTPServer(("", port), Handler)
     print 'Serving media on port %s' % port
